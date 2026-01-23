c4b1d6cedf47ee958ae7e80891ede477f22982b9
Monikhamanjunath <mmonikha7@gmail.com>
2026-01-23 09:03:36 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -39,7 +39,7 @@ public class BankService {
 
         // Perform operations
         acc.deposit(700);
-        System.out.println("Balance after 500 deposit: " + acc.getBalance());
+        System.out.println("Balance after: " + acc.getBalance());
 
         acc.withdraw(100);
         System.out.println("Balance after withdrawal of 300: " + acc.getBalance());
